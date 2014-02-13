a3abcf386f173b5031fe22ae4927957bd9d0c026
Meghann Plunkett <plunkett.meghann@gmail.com>
2014-02-13 16:00:49 -0500

    create attempt broke fetch

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -1,4 +1,5 @@
 var select_nodes = [];
+var heart_nodes = [];
 
 // MODEL
 var Location = Backbone.Model.extend({
@@ -46,24 +47,68 @@ var LocationSelectView = Backbone.View.extend({
 
 });
 
+// HEART BUTTON VIEW
+
+var HeartButtonView = Backbone.View.extend({
+    initialize: function(){
+        var self = this;
+        console.log(this)
+        this.$el.on('click', function(){
+       var favorite = self.$el.parent().find("input").val()
+        })
+    },
+
+   
+})
+
 // COLLECTION
 var LocationCollection = Backbone.Collection.extend({
+
     initialize: function(){
+        var self = this;
         this.fetch();
         this.bind("all", function(e){
             window.location_select_view_1.render();
             window.location_select_view_2.render();
+            self.create();
         });
     },
     url: "/locations",
     model: Location
 });
 
+
+
+/////////////////
 $(function(){
     _.each($("select"), function(dom_node) {
         select_nodes.push(dom_node);
     });
 
+      _.each($(".add-favorite"), function(dom_node) {
+        heart_nodes.push(dom_node);
+    });
+
     window.location_select_view_1 = new LocationSelectView( { el: select_nodes[0]} );
     window.location_select_view_2 = new LocationSelectView( { el: select_nodes[1]} );
+    window.heart_button_view_1 = new HeartButtonView( { el: heart_nodes[0]} );
+    window.heart_button_view_2 = new HeartButtonView( { el: heart_nodes[1]} );
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
